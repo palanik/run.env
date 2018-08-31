17c08a058996a29df547580bc14a6cb3716b3442
Samuel Nilsson <samuel.nilsson@klarna.com>
2018-08-31 11:22:21 +0200

    Add possibility to pass path to env file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import dotenv from 'dotenv';
+import path from 'path';
 import { spawnSync } from 'child_process';
 
-dotenv.config({ silent: true });
-
 const args = process.argv.slice(2);
+const opts = {
+  silent: true,
+};
+
+// Check if a path option has been passed.
+if (args[0] === '-p') {
+  opts.path = path.resolve(process.cwd(), args[1]);
+  // remove both args.
+  args.splice(0, 2);
+}
+
+dotenv.config(opts);
+
 const res = spawnSync(
   args[0],
-  args.slice(1),
-  {
+  args.slice(1), {
     env: process.env,
     stdio: 'inherit',
   },
